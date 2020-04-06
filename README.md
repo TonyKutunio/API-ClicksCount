ce127c380f213803bfdbed4a01de49d366ce5364
TonyKutunio <TonyKutunio@users.noreply.github.com>
2020-04-06 13:45:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+
+# Bitly url shorterer and click counter #
+
+Takes URL and converts it to Bitly-URL and then count the amount of click for all time period.
+
+Python3 should be already installed. Then use pip (or pip3, if there is a conflict with Python2) to install dependencies:
+
+```pip install -r requirements.txt```
+
+
+To get the result you may also need to mention your current python version.
+You can get it by typing in your console:
+ ```$ python --version``` You may see something like: Python 3.8.0.
+Your version may differ.
+
+So you may have to enter something like:
+```python3 main.py (url to be shorten) ```
+
+
+#### Example: 
+```
+$ python(or python3) main.py (url to be shorten) 
+short link : bit.ly/*******
+This link has been cliked : * time(s) 
+``` 
+
+#### Project Goals
+To make life easier working with url-shortening 
---
+++ main.py
@@ -0,0 +1,46 @@
+import requests
+import os
+from dotenv import load_dotenv
+import argparse
+
+BITLY_TOKEN = os.getenv ('BITLY_API')
+
+def shorten_link(token, url):
+  bitlinks_url = 'https://api-ssl.bitly.com/v4/bitlinks'
+  auth_api = {"Authorization": token}
+  long_url = {'long_url': args.url}
+  response = requests.post(bitlinks_url, headers=auth_api, json=long_url)
+  response.raise_for_status()
+  bitlink = response.json()['id']
+  return bitlink
+
+def count_clicks(token, link):
+  clicks_url = 'https://api-ssl.bitly.com/v4/bitlinks/{}/clicks'.format(link)
+  auth_api = {"Authorization": token}
+  info = {'bitlink': args.url,
+          'unit ':'ISO-8601'
+  }
+  response = requests.get(clicks_url, headers=auth_api, params=info)
+  response.raise_for_status()
+  return response.json()
+
+if __name__ == '__main__':
+  load_dotenv()
+  parser = argparse.ArgumentParser(description='converts url to bitly-url')
+  parser.add_argument('url', type=str, help='url to be converted to bitly-url')
+  args = parser.parse_args()
+  if args.url.startswith('bitly.is/') or args.url.startswith('bit.ly/'):
+    try:
+      print('Короткая ссылка :',args.url)
+      print('По вашей ссылки прошли :',count_clicks(BITLY_TOKEN, args.url)['link_clicks'][0]['clicks'],'раз(а)')
+    except requests.exceptions.HTTPError:
+      print('INCORRECT URL')
+  else:
+    shorted = shorten_link(BITLY_TOKEN, args.url)
+    try:
+      print('Короткая ссылка :',shorted)
+      print('По вашей ссылки прошли :',count_clicks(BITLY_TOKEN, shorted)['link_clicks'][0]['clicks'],'раз(а)') 
+    except requests.exceptions.HTTPError:
+      print('INCORRECT URL')
+
+
