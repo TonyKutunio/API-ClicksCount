85434333555f9fb77cab8d984eef00816ee0f5e2
TonyKutunio <55698444+TonyKutunio@users.noreply.github.com>
2020-04-15 11:39:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -24,21 +24,21 @@ def count_clicks(token, link):
 
 if __name__ == '__main__':
   load_dotenv()
-  BITLY_TOKEN = os.getenv('BITLY_API')
+  bitly_token = os.getenv('BITLY_API')
   parser = argparse.ArgumentParser(description='converts url to bitly-url')
   parser.add_argument('url', type=str, help='url to be converted to bitly-url')
   args = parser.parse_args()
   if args.url.startswith('bitly.is/') or args.url.startswith('bit.ly/'):
     try:
       print('Короткая ссылка :',args.url)
-      print('По вашей ссылки прошли :',count_clicks(BITLY_TOKEN, args.url)['link_clicks'][0]['clicks'],'раз(а)')
+      print('По вашей ссылки прошли :',count_clicks(bitly_token, args.url)['link_clicks'][0]['clicks'],'раз(а)')
     except requests.exceptions.HTTPError:
       print('INCORRECT URL')
   else:
-    shorted = shorten_link(BITLY_TOKEN, args.url)
+    shorted = shorten_link(bitly_token, args.url)
     try:
       print('Короткая ссылка :',shorted)
-      print('По вашей ссылки прошли :',count_clicks(BITLY_TOKEN, shorted)['link_clicks'][0]['clicks'],'раз(а)') 
+      print('По вашей ссылки прошли :',count_clicks(bitly_token, shorted)['link_clicks'][0]['clicks'],'раз(а)') 
     except requests.exceptions.HTTPError:
       print('INCORRECT URL')
 
